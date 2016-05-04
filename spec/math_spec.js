df85abcacf0b93a766bbf6b2b7ef38490109f96b
Christina Guintu <Christina Guintu>
2016-05-03 23:32:56 -0400

    added tests and codes for +-*/

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -2,8 +2,21 @@
 var math = require('../math');
 
 describe("A program that does basic arithmetic", function() {
+
   it("can add two numbers together", function() {
     expect(math.add(3,5)).toBe(8);
+  });
+
+  it("can substract two numbers", function() {
+  	expect(math.subtract(3,5)).toBe(-2);
+  });
+
+  it("can multiply two numbers together", function() {
+  	expect(math.multiply(3,5)).toBe(15);
+  });
+
+  it("can divide two numbers", function() {
+  	expect(math.divide(15,5)).toBe(3);
   });
 });
 
